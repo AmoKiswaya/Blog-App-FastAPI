d5be58b78d568b0d5dc490422d4300b47941daea
AmoKiswaya <amosonganga254@gmail.com>
2026-05-20 17:37:11 +0300

    Create templates directory
    Add home route

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request 
+from fastapi.templating import Jinja2Templates 
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates") 
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Amo Kline",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and fast.",
+        "date_posted": "May 20 2026",
+    },
+    {
+        "id": 2,
+        "author": "John Doe",
+        "title": "Python is Awesome",
+        "content": "Python is a great language.",
+        "date_posted": "May 21 2026",
+    },
+]
+
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html") 
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts 
