3c668e36a79bce410919c4aeaa685b891eb69ee3
AmoKiswaya <amosonganga254@gmail.com>
2026-06-10 13:03:31 +0300

    Add template route for getting user's posts

--- main.py
+++ main.py
@@ -38,7 +38,6 @@ def home(request: Request, db: Annotated[Session, Depends(get_db)]):
  
 
 
-
 @app.get("/posts/{post_id}", include_in_schema=False)
 def post_page(request: Request, post_id: int, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.Post).where(models.Post.id == post_id))
@@ -51,6 +50,29 @@ def post_page(request: Request, post_id: int, db: Annotated[Session, Depends(get
             {"post": post, "title": title},
         )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found") 
+
+
+@app.get("/users/{user_id}/posts", include_in_schema=False, name="user_posts")
+def user_posts_page(
+    request: Request,
+    user_id: int,
+    db: Annotated[Session, Depends(get_db)],
+):
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+
+    result = db.execute(select(models.Post).where(models.Post.user_id == user_id))
+    posts = result.scalars().all()
+    return templates.TemplateResponse(
+        request,
+        "user_posts.html",
+        {"posts": posts, "user": user, "title": f"{user.username}'s Posts"},
+    )
 
 
 @app.post(
