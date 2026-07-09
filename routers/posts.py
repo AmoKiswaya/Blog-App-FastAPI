158eb11d7ebdbeec2313345b2deaf171e8abc802
AmoKiswaya <amosonganga254@gmail.com>
2026-07-09 12:52:30 +0300

    Update other routes with current user dependency

--- routers/posts.py
+++ routers/posts.py
@@ -92,6 +92,7 @@ async def update_post_full(
 async def update_post_partial(
     post_id: int,
     post_data: PostUpdate,
+    current_user: CurrentUser,
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     result = await db.execute(select(models.Post).where(models.Post.id == post_id))
@@ -101,6 +102,13 @@ async def update_post_partial(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Post not found",
         )
+    
+    if post.user_id != current_user.id:
+         raise HTTPException(
+            status_code=status.HTTP_403_NOT_FOUND,
+            detail="Not authorized to update this post", 
+        )
+    
 
     update_data = post_data.model_dump(exclude_unset=True)
     for field, value in update_data.items():
@@ -113,7 +121,11 @@ async def update_post_partial(
 
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+async def delete_post(
+    post_id: int,
+    current_user: CurrentUser, 
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
     result = await db.execute(select(models.Post).where(models.Post.id == post_id))
     post = result.scalars().first()
     if not post:
@@ -121,6 +133,12 @@ async def delete_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Post not found",
         )
-
+    
+    if post.user_id != current_user.id:
+         raise HTTPException(
+            status_code=status.HTTP_403_NOT_FOUND,
+            detail="Not authorized to update this post", 
+        )
+    
     await db.delete(post)
     await db.commit() 
