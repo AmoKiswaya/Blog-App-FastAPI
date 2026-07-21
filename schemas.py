ab08393929961226fc285065ee2739ac79ecff1b
AmoKiswaya <amosonganga254@gmail.com>
2026-07-21 14:34:49 +0300

    Add pagination to get_posts route

--- schemas.py
+++ schemas.py
@@ -53,3 +53,10 @@ class PostResponse(PostBase):
     id: int
     date_posted: datetime
     author: UserPublic 
+
+class PaginatedPostsResponse(BaseModel):
+    posts: list[PostResponse]
+    total: int
+    skip: int
+    limit: int
+    has_more: bool
