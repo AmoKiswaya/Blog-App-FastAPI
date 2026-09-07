fa2fdbe960b11a1018812b93947c63191a62e2a0
AmoKiswaya <amosonganga254@gmail.com>
2026-09-07 13:28:44 +0300

    Configure AWS S3
    Add S3 upload and delete functions and update upload profile image and delete profile image to be async

--- config.py
+++ config.py
@@ -14,6 +14,13 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int = 30
 
 
+    # AWS S3 Configuration
+    s3_bucket_name: str
+    s3_region: str = "us-east-1"
+    s3_access_key_id: SecretStr | None = None
+    s3_secret_access_key: SecretStr | None = None
+    s3_endpoint_url: str | None = None
+
     max_upload_size_bytes: int = 5 * 1024 * 1024 
 
     posts_per_page: int = 10
